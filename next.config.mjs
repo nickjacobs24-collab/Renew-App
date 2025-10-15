3bfaaed502ca1feb0d80d9913bd55b1858b18e61
nickjacobs24-collab <nick.jacobs24@gmail.com>
2025-10-15 19:59:45 +0100

    fix: update all image filenames and references

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.optimization.minimize = false;
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
