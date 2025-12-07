4ffb046ce4e97a494359f825a32fca1b71165f62
nickjacobs24-collab <nick.jacobs24@gmail.com>
2025-12-07 15:28:24 +0000

    mobile fix

--- app/results/Modals/Calm/CalmMagnesium.js
+++ app/results/Modals/Calm/CalmMagnesium.js
@@ -145,7 +145,7 @@ const [showWhatToLook, setShowWhatToLook] = useState(false);
       className="absolute text-white rounded-lg whitespace-normal"
 style={{
   width: '280px',
-  bottom: '40px',
+  bottom: '55px',
   right: '0',
   padding: '8px 12px',
   backgroundColor: '#111111',
