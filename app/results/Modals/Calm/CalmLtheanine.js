018fc02d7074e60d64a3bf1b5582b54ec4d53411
nickjacobs24-collab <nick.jacobs24@gmail.com>
2025-12-07 15:18:46 +0000

    mobile fix

--- app/results/Modals/Calm/CalmLtheanine.js
+++ app/results/Modals/Calm/CalmLtheanine.js
@@ -145,8 +145,7 @@ const [showTooltip, setShowTooltip] = useState(false);
     style={{
       width: '280px',
       bottom: '32px',
-      left: '50%',
-      transform: 'translateX(-50%)',
+      left: '0%',
       padding: '8px 12px',
       backgroundColor: '#111111',
       fontSize: '13px',
