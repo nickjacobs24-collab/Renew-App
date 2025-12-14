b9b039e03e068d53ae541571d5cb047fd727446d
Nick Jacobs <nick.jacobs24@gmail.com>
2025-12-14 19:18:52 +0000

    about renew

--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
       <body className="antialiased">
+
+        {/* Google Analytics */}
         {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
           <>
             <Script
@@ -28,6 +29,18 @@ export default function RootLayout({ children }) {
             </Script>
           </>
         )}
+
+        {/* Microsoft Clarity */}
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ulj7v37397");
+          `}
+        </Script>
+
         <Providers>{children}</Providers>
       </body>
     </html>
